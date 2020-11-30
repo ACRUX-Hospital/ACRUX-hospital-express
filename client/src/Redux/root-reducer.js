d090bb91ddb5ec5dd588c66dbbbd447d2251630f
asemnouri <asem_nori@hotmail.com>
2020-11-30 23:20:26 +0200

    redux

--- client/src/Redux/root-reducer.js
+++ client/src/Redux/root-reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import userReducer from './user/userReduser';
+import userReducer from './User/userReduser';
 
 export default combineReducers({
   user: userReducer
